e9f67394781cacc47bd029fc6276a179d8ed1e36
unknown <Bhupesh1Madaan>
2025-11-05 03:34:09 +0530

    changed logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,8 +18,8 @@ const Layout: React.FC<{
         <div className="container mx-auto px-4 py-4">
           <div className="flex justify-between items-center">
             <Link to="/" className="flex items-center">
-              <img alt="AVNTS Logo" className="h-16 mr-2"
-                src="/lovable-uploads/AVNTS-gold-08.png" />
+              <img alt="AVNTS Logo" className="h-24 mr-1"
+                src="/lovable-uploads/AVNTS-Silver-08.png" />
             </Link>
 
             {/* Mobile menu button */}
